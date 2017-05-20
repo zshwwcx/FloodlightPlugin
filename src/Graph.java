670da46b342cab55f2d52ff541e005155eb227b3
haven <834692184@qq.com>
2017-05-20 22:55:07 +0800

    初步完成flow_request类的创建

--- src/Graph.java
+++ src/Graph.java
@@ -96,11 +96,11 @@ class Link{
 }
 
 class Flow_request{
-	String src_id;
-	String dst_id;
+	private String src_id;
+	private String dst_id;
 	int bandwidth_request;
-	int delay_request;
-	int priority;
+	private int delay_request;
+	private int priority;
 	
 	public Flow_request(String src,String dst,int bandwidth,int delay,int priority){
 		this.src_id=src;
@@ -109,7 +109,49 @@ class Flow_request{
 		
 	}
 	
+	public Flow_request(){
+		
+	}
 	
+	public void setSrcId(String src){
+		this.src_id=src;
+	}
+	
+	public void setDstId(String dst){
+		this.dst_id=dst;
+	}
+	
+	public void setBandwidthRequest(int bd){
+		this.bandwidth_request=bd;
+	}
+	
+	public void setDelayRequest(int dl){
+		this.delay_request=dl;
+	}
+	
+	public void setPriority(int pr){
+		this.priority=pr;
+	}
+	
+	public String getSrcId(){
+		return this.src_id;
+	}
+	
+	public String getDstId(){
+		return this.dst_id;
+	}
+	
+	public int getBandwidthRequest(){
+		return this.bandwidth_request;
+	}
+	
+	public int getDelayRequest(){
+		return this.delay_request;
+	}
+	
+	public int getPriority(){
+		return this.priority;
+	}
 	
 }
 
