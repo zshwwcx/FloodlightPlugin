4cda7dfc540b843df4b28aa96e9d42aba556b3f6
haven <834692184@qq.com>
2017-05-31 22:39:24 +0800

    修改了flow request产生函数，为了保证流的有效性，即源节点到目的节点都是可达的，我们在Graph初始化之后的基础上进行Flow Request Generate.

--- src/Graph.java
+++ src/Graph.java
@@ -17,9 +17,9 @@ public class Graph {
 	ArrayList<Link> linklist=new ArrayList<>();//存储图的所有link信息
 	ArrayList<Flow_request> flowRequestList=new ArrayList<>();//存储需要进行TE的所有流需求
 
-    public static String GraphNodeFile="C:\\code\\FloodlightPlugin\\src\\clusters_new_topo";
-    public static String GraphLinkFile="C:\\code\\FloodlightPlugin\\src\\links_new_topo";
-    public static String GraphFlowReuqestListFile="C:\\code\\FloodlightPlugin\\src\\NewFlowRequest.txt";
+    public static String GraphNodeFile="F:\\java code\\src\\topo\\domain4";
+    public static String GraphLinkFile="F:\\java code\\src\\topo\\links_tuple";
+    public static String GraphFlowReuqestListFile="F:\\java code\\src\\flow request\\NewFlowRequest.txt";
 
 	public Graph(String link_file_path,String clusters_file_path){//初始化图，想通过link和clusters两个文件来进行图的初始化，函数参数用文件的String路径来表示，便于后期修改。
 		try{
@@ -162,7 +162,7 @@ public class Graph {
 		output.add(start);
 
 		for(i=1;i<mat.length;i++){
-			min=100000;
+			min=1000000;//此处的min根据实验情况设置为100w，在真实情况中，最大延迟可能更加大，需要根据实际进行调节
 			for(j=0;j<mat.length;j++){
 				if((v[j]==0)&&(dis[j]<min)){
 					min=dis[j];
@@ -336,6 +336,7 @@ public class Graph {
 		}
 
 
+
 	}
 
 	
@@ -411,21 +412,23 @@ public class Graph {
 
 
 	public void FlowRequestFileGenerate(int FlowRequestNumber) throws FileNotFoundException {//FlowRequestNumber为数据流需求文件中需要产生的数据流数目
-		Random rand=new Random(47);
+		Random rand=new Random(43);
 		int FlowSize=this.nodelist.size();
 		try {
             File file_out = new File(GraphFlowReuqestListFile);
             FileWriter file_write=new FileWriter(file_out,false);
             for (int i = 0; i < FlowRequestNumber; i++) {
+
                 int nodeStartNumber=rand.nextInt(FlowSize);
-                int nodeEndNumber=rand.nextInt(FlowSize);
+                int nodeEndNumber=rand.nextInt(nodelist.get(nodeStartNumber).adjcent_list.size());
+                String nodeEndId=this.nodelist.get(nodeStartNumber).adjcent_list.get(nodeEndNumber).adj_id;
                 int bandwidthRequest=rand.nextInt(500);
                 int delayRequest=rand.nextInt(500);
                 int priorityRequest=rand.nextInt(10)+1;
                 if(nodeStartNumber==nodeEndNumber){
                     nodeEndNumber=rand.nextInt(FlowSize);
                 }
-                String content=this.nodelist.get(nodeStartNumber).ID+" "+this.nodelist.get(nodeEndNumber).ID+" "+bandwidthRequest+" "+delayRequest+" "+priorityRequest+"\n";
+                String content=this.nodelist.get(nodeStartNumber).ID+" "+nodeEndId+" "+bandwidthRequest+" "+delayRequest+" "+priorityRequest+"\n";
                 file_write.write(content);
             }
             file_write.close();
@@ -484,7 +487,7 @@ public class Graph {
 			t.showAllocatedPath();
 		}
 		*/
-		//g1.FlowRequestFileGenerate(100);
+		g1.FlowRequestFileGenerate(100);
 		//g1.collectFlowRequest("E:\\代码\\java\\FloodlightPlugin\\src\\Flow Request.txt");
 	 	//g1.localTE();
 	 	g1.run();
